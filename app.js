e14b6ec39cc6aa5e4e562c532b45825b85406027
Shariq Shahbaz <iamsh4r10@gmail.com>
2022-09-11 01:11:12 +0200

    prettier config

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const express =  require('express')
+const express = require('express')
 const app = express()
-const mongoose =  require('mongoose')
+const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const port = 3000
 require('dotenv').config()
 
-const usersController = require('./controllers/users');
+const usersController = require('./controllers/users')
 const authController = require('./controllers/auth')
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.log('Could not connect to MongoDB', err))
-
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Could not connect to MongoDB', err))
 
 app.use(express.json())
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
-    res.json('Hello World')
+  res.json('Hello World')
 })
 
 app.use('/api/users', usersController)
