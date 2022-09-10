05a26be369da8709e277d319f3343fb35ab37d73
Shariq Shahbaz <iamsh4r10@gmail.com>
2022-09-10 21:13:40 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express =  require('express')
+const app = express()
+const mongoose =  require('mongoose')
+const cookieParser = require('cookie-parser')
+const port = 3000
+require('dotenv').config()
+
+const usersController = require('./controllers/users');
+const authController = require('./controllers/auth')
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log('Could not connect to MongoDB', err))
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+    res.json('Hello World')
+})
+
+app.use('/api/users', usersController)
+app.use('/api/auth', authController)
+
+app.listen(port)
---
+++ models/token.schema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const tokenSchema = new Schema(
+  {
+    user: {type: Schema.Types.ObjectId, required: true},
+    token: {type: String, required: true}
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const Token = mongoose.model("Token", tokenSchema);
+module.exports = Token;
